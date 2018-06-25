71796e867bd7ab3dc2dc7c5cf8dcd3ca56828458
Charles Antoine Malenfant <charles_m.18@hotmail.com>
2018-06-25 15:37:18 -0400

    First ever commit, app created using Android Studio targetting API 26 for Android phones and tablets. No Kotlin or C++ support and no other Android Studio features have been selected. Package name is the domain.com domain. Esketit!

---
+++ app/src/main/java/malenerdgaming/com/flunkyball/MainActivity.java
@@ -0,0 +1,13 @@
+package malenerdgaming.com.flunkyball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
